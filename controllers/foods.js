e6437cbaff2b10eca17b9891f82108c330ec316b
Shaina G <shainagtherapy@gmail.com>
2025-07-24 22:33:19 -0700

    GET / NEW / POST / SHOW PROGRESS- still cannot get to 'new' template

--- controllers/foods.js
+++ controllers/foods.js
@@ -2,26 +2,40 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user.js');
 
-// INDEX
-router.get('/', async (req, res) => {
+// GET / for login index
+router.get('/', (req, res) => {
+    res.render('foods/index.ejs');
+})
+
+// NEW
+router.get('/new', (req, res) => {
+    res.render('foods/new.ejs')
+});
+
+// POST / create (submit new item)
+router.post('/', async (req, res) => {
+    const currentUser = await User.findById(req.session.user._id);
+
+    currentUser.foods.push(req.body);
+
+    await currentUser.save();
+
+    res.redirect(`/users/${currentUser._id}/foods`)
+})
+
+// SHOW / reveal food pantry index
+router.get('/:foodId', async (req, res) => {
     try {
         const currentUser = await User.findById(req.session.user._id);
-
-        res.render('foods/index.ejs', {
-            foods: currentUser.foods,
+        const food = currentUser.foods.id(req.params.foodsId);
+        res.render('foods/show.ejs', {
+            food: food,
         })
     } catch (error) {
         console.log(error);
         res.redirect('/');
     }
-})
-
-// NEW
-router.get('/new', async (req, res) => {
-    res.render('foods/new.ejs')
-})
-
-
+});
 
 
 
