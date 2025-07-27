913279f78db54c2eba959fe8c1688382ef1584c5
Shaina G <shainagtherapy@gmail.com>
2025-07-26 20:29:27 -0700

    Community User Links activated, still working on show.ejs

--- controllers/communityUsers.js
+++ controllers/communityUsers.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 
-// GET VIEW / DIRECTORY FOR ALL USERS
+// GET index / DIRECTORY FOR ALL USERS
 router.get("/communityUsers", async (req, res) => {
   try {
-    const allUsers = await User.find({});
+    const allUsers = await User.find(req.params.username);
     res.render("communityUsers/index.ejs", {
       communityUsers: allUsers,
     });
@@ -13,6 +13,21 @@ router.get("/communityUsers", async (req, res) => {
     console.log(error);
     res.redirect("/");
   }
+});
+
+// SHOW ROUTE - links to communityUsers
+router.get('/:commUserId', async (req, res) => {
+    try {
+        const commUser = await User.findById(req.params.username); //_id or username?
+        //const commPantry = commUser.pantry.id(req.params.pantryId);
+        res.render('communityUsers/show.ejs', {
+            commUser: commUser,
+        });
+    } catch (error) {
+        // If any errors, log them and redirect back home
+        console.log(error);
+        res.redirect('/communityUsers');
+    }
 });
 
 module.exports = router;
